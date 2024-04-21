bfa058d736e812de6568c81f702294210c44c8ab
Clyde0513 <iclyde.villacrusis@gmail.com>
2024-04-20 18:28:24 -0700

    fixed routing

--- hiking-app/src/App.js
+++ hiking-app/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Logo from './logo';
-import BGTrees from './bgtrees';
 import Menu from './Menu'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from "./pages/about"
@@ -10,11 +9,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          {/* <BGTrees/> */}
          
       <header className=  "App-header">
       <Logo/>
-      {/* <Menu/> */}
       <Routes >
             <Route path ="/"element = {<Menu/>}/>
             <Route path ="/about"element = {<About/>}/>
@@ -22,18 +19,6 @@ function App() {
         <div className="menu">
         
         </div>
-
-       
-
-          {/* <label>username</label>
-          <input class="w3-input" type="text"></input>
-
-          <label>password</label>
-          <input class="w3-input" type="text"></input><br></br>
-    
-          <button>sign in</button> */}
-
-         
                 
       </header>
       </BrowserRouter>
